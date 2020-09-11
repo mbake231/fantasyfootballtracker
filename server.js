3fa6749a210b953197c0128acaaf63f9abc9e966
mikeehinger <mehinger@gmail.com>
2020-09-10 20:09:47 -0400

    m2

--- server.js
+++ server.js
@@ -3,15 +3,10 @@ var mongo = require('mongodb');
 
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3001;
+  port = process.env.PORT || 3000;
 
 app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
 
-
-app.get("/data", (req, res, next) => {
-  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
- });
-
-
+app.use(express.static(path.join(__dirname, 'build')));
