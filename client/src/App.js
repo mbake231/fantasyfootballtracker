69b5bdc6cfeedf827cb1d114efd7462d1593cd6b
mikeehinger <mehinger@gmail.com>
2020-09-12 15:16:33 -0400

    refresh

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,7 @@ render() {
   }}>
     <div id="AppContainer">
 
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="dark" variant="dark" expand="lg">
   <Navbar.Brand href="/">League of Hunks</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
